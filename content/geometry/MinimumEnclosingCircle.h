e7085bcc14a39fd917b525a8481faebe7ee8e7af
mohamedsobhi777 <33491613+mohamedsobhi777@users.noreply.github.com>
2021-10-17 20:56:07 +0200

    Update MinimumEnclosingCircle.h

--- content/geometry/MinimumEnclosingCircle.h
+++ content/geometry/MinimumEnclosingCircle.h
@@ -12,19 +12,22 @@
 #include "circumcircle.h"
 
 pair<P, double> mec(vector<P> ps) {
-	shuffle(all(ps), mt19937(time(0)));
-	P o = ps[0];
-	double r = 0, EPS = 1 + 1e-8;
-	rep(i,0,sz(ps)) if ((o - ps[i]).dist() > r * EPS) {
-		o = ps[i], r = 0;
-		rep(j,0,i) if ((o - ps[j]).dist() > r * EPS) {
-			o = (ps[i] + ps[j]) / 2;
-			r = (o - ps[i]).dist();
-			rep(k,0,j) if ((o - ps[k]).dist() > r * EPS) {
-				o = ccCenter(ps[i], ps[j], ps[k]);
-				r = (o - ps[i]).dist();
-			}
-		}
-	}
-	return {o, r};
+       shuffle( ps.begin(), ps.end(), mt19937(time(0)));
+       P o = ps[0];
+       double r = 0, EPS = 1 + 1e-8;
+       for(int i = 0 ; i < (int) ps.size() ; ++i) if ((o - ps[i]).dist() > r * EPS) {
+              o = ps[i], r = 0;
+              for(int j = 0 ;j < i; ++ j)
+                     if ((o - ps[j]).dist() > r * EPS) {
+                            o = (ps[i] + ps[j]) / 2;
+                            r = (o - ps[i]).dist();
+                            for(int k = 0 ; k < j ; ++ k){
+                                   if ((o - ps[k]).dist() > r * EPS) {
+                                          o = ccCenter(ps[i], ps[j], ps[k]);
+                                          r = (o - ps[i]).dist();
+                                   }   
+                            }
+                     }
+       }
+       return {o, r};
 }
